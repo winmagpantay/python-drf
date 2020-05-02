32b64f40638dda695e1355cc4235a27cd62eecc0
arlenemariano <arlene.codictted@gmail.com>
2020-05-02 16:14:27 +0800

    Initial Virtual ENV

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\63939\documents\python\winmagpantay\python-drf\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
